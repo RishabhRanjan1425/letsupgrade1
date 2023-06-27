c694df2cf03472a14a0090c09520a91c691b6b08
RishabhRanjan1425 <115249026+RishabhRanjan1425@users.noreply.github.com>
2023-06-27 21:20:14 +0530

    Add files via upload

---
+++ Practice opertors.py
@@ -0,0 +1,94 @@
+#Arithematic opertor
+'''+'''
+#1when Two Integers are added
+a=6+7
+print(a)
+#2Whn one interger and complex number is added
+b=7+(5+6j)
+print(b)
+#3When one interger and floating dgit is added
+c=8+.88
+print(c)
+#4When logic and integer is added
+d=True+5
+e=False+7
+print(d)
+print(e)
+#5When Both string is added
+f="Hello"+"World"
+print(f)
+#6When we add something to tuple
+g=["Hello"]+["SUN , MOON , WORLD "]
+print(g)
+'''-'''
+#1When both are integer
+h=78-46
+print(h)
+'''*'''
+#1When two ineger multiplies
+i=8*8
+print(i)
+#2When both are boolean value
+j=0.8*4.9
+#3Whwn one is integer and other is str
+k=5*"Hi"
+print(k)
+#When one is int and other is list/Tuple
+l=7*[22,33,44,55]
+print(l)
+'''/'''
+#1When both are integer
+m1=84/7
+m2=64/5
+print(m1)
+print(m2)
+
+'''//'''
+#1When both are integer
+n=67//7
+print(n)
+#2When one is float and other is int
+o=8.1//9
+print(o)
+#3When both are float
+p=8.4//0.7
+print(p)
+'''%'''
+#1When both are integer
+q=35%8
+print(q)
+#Logical operator
+#and
+#1When both are operands are true
+r = 78 and 88
+print(r)
+#2When one is true and other is false
+s= "" and 6
+print(s)
+#3When both are false
+t=""and""
+print(t)
+#OR
+#1When both are true
+u=98 or 34
+print(u)
+#2 When one are true and other is false
+v="" or 76
+print(v)
+#3 When both are false
+w= "" or  ''
+print(w)
+#not
+#1 When both are true
+x = not 76
+print(x)
+#2 When  both are false
+y= not 77
+print(y)
+#Assignment Operators
+a1=56
+a1+=22
+print(a1)
+a1-=6
+print(a1)
+
